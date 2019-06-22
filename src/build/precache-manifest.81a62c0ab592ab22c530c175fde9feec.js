c2bc9c74d259860f93bb3bec9eb34596089bd067
Jeyakumaran Mayooresan <mayu3g@gmail.com>
2019-06-23 00:40:09 +0530

    git blog first commit

---
+++ src/build/precache-manifest.81a62c0ab592ab22c530c175fde9feec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4cf92e81fd46aec538db19a363399fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d4397c826efb90d9828",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "d72083eee934c8f52390",
+    "url": "/static/js/2.11048f7f.chunk.js"
+  },
+  {
+    "revision": "3d4397c826efb90d9828",
+    "url": "/static/js/main.32e4d63f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
